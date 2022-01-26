1de84b7ff3ac2afe2a2421445e8c9a1929654bd4
martipurull <91315241+martipurull@users.noreply.github.com>
2022-01-26 17:57:19 +0100

    adds user functionality to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -31,29 +31,36 @@ function Home({ user }) {
         <Container>
             <Row className="mt-4">
                 <Col xs={8}>
-                    <Form className="d-flex">
-                        <FormControl
-                            type="search"
-                            placeholder="find your job"
-                            className="me-2"
-                            aria-label="Search"
-                            onChange={e => setSearchTerm(e.target.value)}
-                        />
-                    </Form>
+                    {user &&
+                        <Form className="d-flex">
+                            <FormControl
+                                type="search"
+                                placeholder="find your job"
+                                className="me-2"
+                                aria-label="Search"
+                                onChange={e => setSearchTerm(e.target.value)}
+                            />
+                        </Form>
+                    }
                 </Col>
                 <Col>
-                    <Form.Select aria-label="Default select example" onChange={e => setSelectedCategory(e.currentTarget.value)}>
-                        <option value="">By category</option>
-                        {categories && categories.map((c, i) => (
-                            <option key={i} value={c} >{c}</option>
-                        ))}
-                        <option value="">No category</option>
-                    </Form.Select>
+                    {user &&
+                        <Form.Select aria-label="Default select example" onChange={e => setSelectedCategory(e.currentTarget.value)}>
+                            <option value="">By category</option>
+                            {categories && categories.map((c, i) => (
+                                <option key={i} value={c} >{c}</option>
+                            ))}
+                            <option value="">No category</option>
+                        </Form.Select>
+                    }
                 </Col>
             </Row>
             <Row>
                 <Col></Col>
-                <Col xs={8}><JobsList searchTerm={debouncedSearchTerm} selectedCategory={selectedCategory} /></Col>
+                <Col xs={6}>
+                    {user && <JobsList searchTerm={debouncedSearchTerm} selectedCategory={selectedCategory} />}
+                    {!user && <h4 className='mt-5'>Please enter a username to start searching for jobs.</h4>}
+                </Col>
                 <Col></Col>
             </Row>
         </Container>
