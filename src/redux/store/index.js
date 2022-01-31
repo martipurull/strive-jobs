cf50d6486eb749e31c7506747737f7f53825782c
martipurull <91315241+martipurull@users.noreply.github.com>
2022-01-31 14:21:43 +0100

    adds encryption

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -3,6 +3,7 @@ import { favouritesReducer, jobsReducer, userReducer } from '../reducers'
 import thunk from 'redux-thunk'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage/session'
+import { encryptTransform } from 'redux-persist-transform-encrypt'
 
 const ultimateCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
@@ -20,7 +21,16 @@ export const initialState = {
     }
 }
 
-const persistConfig = { key: 'root', storage }
+const persistConfig = {
+    key: 'root',
+    storage,
+    transforms: [
+        encryptTransform({
+            secretKey: process.env.REACT_APP_SECRET_KEY,
+            onError: (error) => console.log('encryption error: ', error)
+        }),
+    ]
+}
 
 const mainReducer = combineReducers({
     companies: favouritesReducer,
